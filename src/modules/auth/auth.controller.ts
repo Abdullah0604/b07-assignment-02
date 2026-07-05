836a1a64e05dbbe5e913bad7c89d2670c692f7db
greenFruits <mdabdullah6548ctg@gmail.com>
2026-07-05 21:40:02 +0600

    auth controller is added for sign up

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { userDataValidation } from "../../utilities/userdataValidation.js";
 import sendError from "../../utilities/sendError.js";
 import authServices from "./auth.service.js";
+import sendSuccess from "../../utilities/sendSuccess.js";
 
 const createUser = async (req: Request, res: Response) => {
   const validatedMessage = userDataValidation(req.body);
@@ -10,8 +11,30 @@ const createUser = async (req: Request, res: Response) => {
   }
 
   try {
-    const result = await authServices.loginUserIntoDB();
-  } catch (error: any) {}
+    const result = await authServices.createUserIntoDB(req.body);
+
+    // console.log("create user:  ", result);
+    if (result.rows.length) {
+      return sendSuccess(
+        res,
+        201,
+        "User registered successfully",
+        result.rows[0],
+      );
+    }
+  } catch (error: any) {
+    // console.log(error);
+    if (error.code === "23505") {
+      return sendError(
+        res,
+        400,
+        "Duplicate value",
+        "Duplicate email is not acceptable. Email must be unique. Please provide an unique email.",
+      );
+    }
+
+    return sendError(res, 500, "Internal server error", error.message);
+  }
 };
 
 const loginUser = async (req: Request, res: Response) => {};
