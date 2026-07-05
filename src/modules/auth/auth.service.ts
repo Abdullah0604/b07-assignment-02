87f5add66ce3a56926381e62b700814e34c190d3
greenFruits <mdabdullah6548ctg@gmail.com>
2026-07-05 21:36:28 +0600

    feat: integrate database in auth service for sign up

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,18 @@
-const createUserIntoDB = async () => {};
+import bcrypt from "bcryptjs";
+import { pool } from "../../config/db.js";
+
+const createUserIntoDB = async (payload: Record<string, unknown>) => {
+  const { name, email, password, role } = payload;
+
+  const hasedPassword = await bcrypt.hash(password as string, 10);
+  const result = await pool.query(
+    "INSERT INTO users (name, email, password, role) VALUES($1,$2,$3,$4) RETURNING *",
+    [name, email, hasedPassword, role],
+  );
+
+  delete result.rows[0].password;
+  return result;
+};
 const loginUserIntoDB = async () => {};
 const authServices = {
   createUserIntoDB,
