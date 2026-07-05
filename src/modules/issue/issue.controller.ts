e57df46b57992c0e507b21f79a326be5b2947b8b
greenFruits <mdabdullah6548ctg@gmail.com>
2026-07-05 23:47:10 +0600

    setup: set all basic structure for issue's feature

--- src/modules/issue/issue.controller.ts
+++ src/modules/issue/issue.controller.ts
@@ -1,3 +1,21 @@
-const issueControllers = {};
+import type { Request, Response } from "express";
+
+const createIssue = async (req: Request, res: Response) => {};
+
+const getAllIssue = async (req: Request, res: Response) => {};
+
+const getSingleIssue = async (req: Request, res: Response) => {};
+
+const updateIssue = async (req: Request, res: Response) => {};
+
+const deleteIssue = async (req: Request, res: Response) => {};
+
+const issueControllers = {
+  createIssue,
+  getAllIssue,
+  getSingleIssue,
+  updateIssue,
+  deleteIssue,
+};
 
 export default issueControllers;
--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -1,3 +1,19 @@
-const issueServices = {};
+const createIssueIntoDB = async () => {};
+
+const getAllIssueFromDB = async () => {};
+
+const getSingleIssueFromDB = async () => {};
+
+const updateIssueIntoDB = async () => {};
+
+const deleteIssueFromDB = async () => {};
+
+const issueServices = {
+  createIssueIntoDB,
+  getAllIssueFromDB,
+  getSingleIssueFromDB,
+  updateIssueIntoDB,
+  deleteIssueFromDB,
+};
 
 export default issueServices;
